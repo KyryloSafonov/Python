958ddded48932e7727485ef580d3a67fe8ac8767
KyryloSafonov <kirilsafon@gmail.com>
2021-02-24 20:52:57 +0200

    Initial commit

---
+++ Lesson1.py
@@ -0,0 +1,54 @@
+# Task 1
+# list = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
+# print(min(list))
+
+# Task 2
+# list_ = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
+#
+# index = len(list_)-1
+# while index:
+#     item = list_[index]
+#     if list_.count(item) >1:
+#         while list_.count(item)>0:
+#             list_.remove(item)
+#             index-=1
+#     else:
+#         index-=1
+#
+# print(list_)
+
+# Task 2 (alternative)
+# list_ = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
+#
+# index = len(list_)-1
+# while index:
+#     item = list_[index]
+#     if list_.count(item) >1:
+#         while list_.count(item)>1:
+#             list_.remove(item)
+#             index-=1
+#     else:
+#         index-=1
+#
+# print(list_)
+
+# Task 3
+# list_ = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5, 6, 7, 10, 11]
+#
+# for i in range(3,len(list_),4):
+#    list_[i]='X'
+# print(list_)
+
+#Task 4
+list_ = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
+
+
+
+
+
+
+
+
+
+
+
